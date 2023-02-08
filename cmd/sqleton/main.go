fa81bd2691c6de549bb9e17ea2c5052ee1a787b4
Manuel Odendahl <wesen@ruinwesen.com>
2023-02-08 00:15:43 -0500

    :recycle: Move init and load commands code to clay

--- cmd/sqleton/main.go
+++ cmd/sqleton/main.go
@@ -3,18 +3,13 @@ package main
 import (
 	"embed"
 	"fmt"
-	glazed_cmds "github.com/go-go-golems/glazed/pkg/cmds"
+	clay "github.com/go-go-golems/clay/pkg"
 	"github.com/go-go-golems/glazed/pkg/help"
 	"github.com/go-go-golems/sqleton/cmd/sqleton/cmds"
-	sqleton "github.com/go-go-golems/sqleton/pkg"
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
+	"github.com/go-go-golems/sqleton/pkg"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"gopkg.in/natefinch/lumberjack.v2"
-	"io"
 	"os"
-	"strings"
 )
 
 var rootCmd = &cobra.Command{
@@ -23,212 +18,9 @@ var rootCmd = &cobra.Command{
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		// reinitialize the logger because we can now parse --log-level and co
 		// from the command line flag
-		initLogger()
+		err := clay.InitLogger()
+		cobra.CheckErr(err)
 	},
-}
-
-func initLogger() {
-	logLevel := viper.GetString("log-level")
-	verbose := viper.GetBool("verbose")
-	if verbose && logLevel != "trace" {
-		logLevel = "debug"
-	}
-
-	err := InitLogger(&logConfig{
-		Level:      logLevel,
-		LogFile:    viper.GetString("log-file"),
-		LogFormat:  viper.GetString("log-format"),
-		WithCaller: viper.GetBool("with-caller"),
-	})
-	cobra.CheckErr(err)
-}
-
-func initCommands(
-	rootCmd *cobra.Command, configPath string, helpSystem *help.HelpSystem,
-) ([]*sqleton.SqlCommand, []*glazed_cmds.CommandAlias, error) {
-	// Load the variables from the environment
-	viper.SetEnvPrefix("sqleton")
-
-	if configPath != "" {
-		viper.SetConfigFile(configPath)
-	} else {
-		viper.AddConfigPath(".")
-		viper.AddConfigPath("$HOME/.sqleton")
-		viper.AddConfigPath("/etc/sqleton")
-
-		xdgConfigPath, err := os.UserConfigDir()
-		if err == nil {
-			viper.AddConfigPath(xdgConfigPath + "/sqleton")
-		}
-	}
-
-	// Read the configuration file into Viper
-	err := viper.ReadInConfig()
-	// if the file does not exist, continue normally
-	if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-		// Config file not found; ignore error
-	} else if err != nil {
-		// Config file was found but another error was produced
-		return nil, nil, err
-	}
-	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
-	viper.AutomaticEnv()
-
-	// Bind the variables to the command-line flags
-	err = viper.BindPFlags(rootCmd.PersistentFlags())
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// this still won't pick up on --verbose to show debug logging when the commands
-	// are parsed, but at least it will configure it based on the config file
-	initLogger()
-
-	log.Debug().
-		Str("config", viper.ConfigFileUsed()).
-		Msg("Loaded configuration")
-
-	loader := &sqleton.SqlCommandLoader{}
-	var commands []*sqleton.SqlCommand
-	commands_, aliases, err := glazed_cmds.LoadCommandsFromFS(loader, queriesFS, "embed", ".", "queries/")
-	if err != nil {
-		return nil, nil, err
-	}
-	for _, command := range commands_ {
-		commands = append(commands, command.(*sqleton.SqlCommand))
-	}
-
-	err = helpSystem.LoadSectionsFromFS(queriesFS, "queries/doc")
-	if err != nil {
-		return nil, nil, err
-	}
-
-	repositoryCommands, repositoryAliases, err := loadRepositoryCommands(helpSystem)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	commands = append(commands, repositoryCommands...)
-	aliases = append(aliases, repositoryAliases...)
-
-	var cobraCommands []glazed_cmds.CobraCommand
-	for _, command := range commands {
-		cobraCommands = append(cobraCommands, command)
-	}
-
-	err = glazed_cmds.AddCommandsToRootCommand(rootCmd, cobraCommands, aliases)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return commands, aliases, nil
-}
-
-func loadRepositoryCommands(helpSystem *help.HelpSystem) ([]*sqleton.SqlCommand, []*glazed_cmds.CommandAlias, error) {
-	repositories := viper.GetStringSlice("repositories")
-
-	loader := &sqleton.SqlCommandLoader{}
-
-	defaultDirectory := "$HOME/.sqleton/queries"
-	repositories = append(repositories, defaultDirectory)
-
-	commands := make([]*sqleton.SqlCommand, 0)
-	aliases := make([]*glazed_cmds.CommandAlias, 0)
-
-	for _, repository := range repositories {
-		repository = os.ExpandEnv(repository)
-
-		// check that repository exists and is a directory
-		s, err := os.Stat(repository)
-
-		if os.IsNotExist(err) {
-			log.Debug().Msgf("Repository %s does not exist", repository)
-			continue
-		} else if err != nil {
-			log.Warn().Msgf("Error while checking directory %s: %s", repository, err)
-			continue
-		}
-
-		if s == nil || !s.IsDir() {
-			log.Warn().Msgf("Repository %s is not a directory", repository)
-		} else {
-			docDir := fmt.Sprintf("%s/doc", repository)
-			commands_, aliases_, err := glazed_cmds.LoadCommandsFromFS(loader, os.DirFS(repository), "file", ".", repository)
-			if err != nil {
-				return nil, nil, err
-			}
-			for _, command := range commands_ {
-				commands = append(commands, command.(*sqleton.SqlCommand))
-			}
-			aliases = append(aliases, aliases_...)
-
-			_, err = os.Stat(docDir)
-			if os.IsNotExist(err) {
-				continue
-			} else if err != nil {
-				log.Debug().Err(err).Msgf("Error while checking directory %s", docDir)
-				continue
-			}
-			err = helpSystem.LoadSectionsFromFS(os.DirFS(docDir), ".")
-			if err != nil {
-				log.Warn().Err(err).Msgf("Error while loading help sections from directory %s", repository)
-				continue
-			}
-		}
-	}
-	return commands, aliases, nil
-}
-
-type logConfig struct {
-	WithCaller bool
-	Level      string
-	LogFormat  string
-	LogFile    string
-}
-
-func InitLogger(config *logConfig) error {
-	if config.WithCaller {
-		log.Logger = log.With().Caller().Logger()
-	}
-	// default is json
-	var logWriter io.Writer
-	if config.LogFormat == "text" {
-		logWriter = zerolog.ConsoleWriter{Out: os.Stderr}
-	} else {
-		logWriter = os.Stderr
-	}
-
-	if config.LogFile != "" {
-		logWriter = io.MultiWriter(
-			logWriter,
-			zerolog.ConsoleWriter{
-				NoColor: true,
-				Out: &lumberjack.Logger{
-					Filename:   config.LogFile,
-					MaxSize:    10, // megabytes
-					MaxBackups: 3,
-					MaxAge:     28,    //days
-					Compress:   false, // disabled by default
-				},
-			})
-	}
-
-	log.Logger = log.Output(logWriter)
-
-	switch config.Level {
-	case "debug":
-		zerolog.SetGlobalLevel(zerolog.DebugLevel)
-	case "info":
-		zerolog.SetGlobalLevel(zerolog.InfoLevel)
-	case "warn":
-		zerolog.SetGlobalLevel(zerolog.WarnLevel)
-	case "error":
-		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
-	case "fatal":
-		zerolog.SetGlobalLevel(zerolog.FatalLevel)
-	}
-
-	return nil
 }
 
 func main() {
@@ -288,7 +80,6 @@ func init() {
 	rootCmd.PersistentFlags().String("dsn", "", "Database DSN")
 	rootCmd.PersistentFlags().String("driver", "", "Database driver")
 
-	rootCmd.PersistentFlags().String("config", "", "Path to config file (default ~/.sqleton/config.yml)")
 	rootCmd.PersistentFlags().Bool("verbose", false, "Verbose output")
 
 	rootCmd.AddCommand(cmds.DbCmd)
@@ -299,22 +90,46 @@ func init() {
 
 	cmds.InitializeMysqlCmd(queriesFS, helpSystem)
 
-	// parse the flags one time just to catch --config
-	configFile := ""
-	for idx, arg := range os.Args {
-		if arg == "--config" {
-			if len(os.Args) > idx+1 {
-				configFile = os.Args[idx+1]
-			}
-		}
+	err = clay.InitViper("sqleton", rootCmd)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Error initializing config: %s\n", err)
+		os.Exit(1)
+	}
+	err = clay.InitLogger()
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Error initializing logger: %s\n", err)
+		os.Exit(1)
 	}
 
-	commands, aliases, err := initCommands(rootCmd, configFile, helpSystem)
+	repositories := viper.GetStringSlice("repositories")
+
+	defaultDirectory := "$HOME/.sqleton/queries"
+	repositories = append(repositories, defaultDirectory)
+
+	locations := clay.CommandLocations{
+		Embedded: []clay.EmbeddedCommandLocation{
+			{
+				FS:      queriesFS,
+				Name:    "embed",
+				Root:    "queries/",
+				DocRoot: "queries/doc",
+			},
+		},
+		Repositories: repositories,
+	}
+
+	commands, aliases, err := locations.LoadCommands(
+		&pkg.SqlCommandLoader{}, helpSystem, rootCmd)
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "Error initializing commands: %s\n", err)
 		os.Exit(1)
 	}
 
-	queriesCmd := cmds.AddQueriesCmd(commands, aliases)
+	sqlCommands, ok := clay.CastList[*pkg.SqlCommand](commands)
+	if !ok {
+		_, _ = fmt.Fprintf(os.Stderr, "Error initializing commands: %s\n", err)
+		os.Exit(1)
+	}
+	queriesCmd := cmds.AddQueriesCmd(sqlCommands, aliases)
 	rootCmd.AddCommand(queriesCmd)
 }
--- go.mod
+++ go.mod
@@ -3,15 +3,15 @@ module github.com/go-go-golems/sqleton
 go 1.19
 
 require (
+	github.com/go-go-golems/clay v0.0.0-20230208051353-c4c2f51311e7
 	github.com/go-go-golems/glazed v0.2.3
 	github.com/go-sql-driver/mysql v1.7.0
 	github.com/huandu/go-sqlbuilder v1.18.0
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/pkg/errors v0.9.1
-	github.com/rs/zerolog v1.28.0
+	github.com/rs/zerolog v1.29.0
 	github.com/spf13/cobra v1.6.1
-	github.com/spf13/viper v1.14.0
-	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+	github.com/spf13/viper v1.15.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
@@ -37,7 +37,7 @@ require (
 	github.com/kopoli/go-terminal-size v0.0.0-20170219200355-5c97524c8b54 // indirect
 	github.com/lib/pq v1.10.4 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
@@ -49,14 +49,14 @@ require (
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.13.0 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/scylladb/termtables v0.0.0-20191203121021-c4c0b6d42ff4 // indirect
-	github.com/spf13/afero v1.9.2 // indirect
+	github.com/spf13/afero v1.9.3 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/subosito/gotenv v1.4.1 // indirect
+	github.com/subosito/gotenv v1.4.2 // indirect
 	github.com/tj/go-naturaldate v1.3.0 // indirect
 	github.com/yuin/goldmark v1.5.4 // indirect
 	github.com/yuin/goldmark-emoji v1.0.1 // indirect
@@ -65,5 +65,6 @@ require (
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
